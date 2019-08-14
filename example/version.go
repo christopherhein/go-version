07e4181c123766887af9f2c61679057b6216f8be
Christopher Hein <me@chrishein.com>
2019-08-13 18:31:30 -1000

    adding yaml outputter
    Signed-off-by: Christopher Hein <me@chrishein.com>

--- example/version.go
+++ example/version.go
@@ -16,7 +16,7 @@ limitations under the License.
 package main
 
 import (
-	"os"
+	"fmt"
 
 	"github.com/spf13/cobra"
 	goVersion "go.hein.dev/go-version"
@@ -27,15 +27,21 @@ var (
 	version    = "dev"
 	commit     = "none"
 	date       = "unknown"
+	output     = "json"
 	versionCmd = &cobra.Command{
 		Use:   "version",
 		Short: "Version will output the current build information",
 		Long:  ``,
-		Run:   goVersion.Func(os.Stdout, shortened, version, commit, date),
+		Run: func(_ *cobra.Command, _ []string) {
+			resp := goVersion.FuncWithOutput(shortened, version, commit, date, output)
+			fmt.Print(resp)
+			return
+		},
 	}
 )
 
 func init() {
-	versionCmd.Flags().BoolVarP(&shortened, "short", "s", false, "Use shortened output for version information.")
+	versionCmd.Flags().BoolVarP(&shortened, "short", "s", false, "Print just the version number.")
+	versionCmd.Flags().StringVarP(&output, "output", "o", "json", "Output format. One of 'yaml' or 'json'.")
 	rootCmd.AddCommand(versionCmd)
 }
